8bdb34291377570d75352ab85f9947e24a68200c
Jayson <jayson_tolentino@efi.net.ph>
2021-01-19 08:37:19 +0800

    updated ci app template with user authentication

--- application/views/partials/navbar.php
+++ application/views/partials/navbar.php
@@ -19,7 +19,6 @@
       </li>
     </ul>
 
-
     <ul class="navbar-nav ml-auto">
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
